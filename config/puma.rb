227926a2b3f449b126f007697a545060abc38fd9
Dylan Smith <foliomedia@gmail.com>
2013-06-02 08:24:42 -0700

    wip

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,7 @@ app_name = env_apps_map[env.to_sym]
 
 threads 4,4
 
-bind  "unix:///home/deploy/apps/#{app_name}/tmp/puma/#{app_name}-puma.sock"
+bind  "unix:///home/deploy/apps/#{app_name}/current/puma/#{app_name}-puma.sock"
 pidfile "/home/deploy/apps/#{app_name}/current/tmp/puma/pid/#{env}.pid"
 state_path "/home/deploy/apps/#{app_name}/current/tmp/puma/state/#{env}.state"
 
