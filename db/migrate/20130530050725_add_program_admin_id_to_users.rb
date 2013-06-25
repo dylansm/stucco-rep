4e5964aeb391909b4b87a286873887dcc1187a58
Dylan Smith <foliomedia@gmail.com>
2013-06-25 14:00:00 -0700

    New models: posts, comments.

--- db/migrate/20130530050725_add_program_admin_id_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddProgramAdminIdToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :program_admin_id, :integer
-  end
-end
--- db/migrate/20130612161250_remove_program_admins_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProgramAdminsFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :program_admin_id, :integer
-  end
-end
