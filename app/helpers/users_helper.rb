599a60649f2ebc3d46a730a74cc5b36d973e324f
Dylan Smith <foliomedia@gmail.com>
2013-09-09 10:11:45 -0700

    Moved devise/sessions/new to partial (including it in new)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,6 +7,10 @@ module UsersHelper
     @resource ||= User.new
   end
 
+  def resource_class
+    devise_mapping.to
+  end
+
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
